4cd2491404533f8be81637e04bf23f48c22615e2
Alassane NDIAYE <andiaye@modelsis.sn>
2022-09-06 07:44:49 +0000

    init

---
+++ src/test/java/com/mndevops/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mndevops.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
